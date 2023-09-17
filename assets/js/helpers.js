b8960b090119406d30c92ca8bda246e5194b24fd
Dalia Alawneh <alawnehdalia340@gmail.com>
2023-09-17 17:56:01 +0300

    test commit

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -1,14 +1,1 @@
-function showModal() {
-    addModal.classList.remove('hide-modal');
-    document.querySelector('.overlay').classList.remove('hide-overlay');
-    // if (selectedColor) {
-    //     addNoteButton.style.backgroundColor = `var(--color-${selectedColor})`;
-    // }
-}
-
-
-function hideModal() {
-    addModal.classList.add('hide-modal');
-    document.querySelector('.overlay').classList.add('hide-overlay');
-    // addNoteButton.style.backgroundColor = ''; 
-}
+function showModal(){addModal.classList.remove("hide-modal"),document.querySelector(".overlay").classList.remove("hide-overlay")}function hideModal(){addModal.classList.add("hide-modal"),document.querySelector(".overlay").classList.add("hide-overlay")}
